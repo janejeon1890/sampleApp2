b4f41a5f324d90147ee8dec2a4dbce2998b00a07
jane.jeon1890 <jane.jeon1890@kakaocorp.com>
2019-05-10 15:03:30 +0900

    Updates

---
+++ precache-manifest.53d1c6ee0b5df539568009dc42ba0ba3.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb52fac07383f041dd4a",
+    "url": "/sampleApp2/static/css/main.238c8f4d.chunk.css"
+  },
+  {
+    "revision": "fb52fac07383f041dd4a",
+    "url": "/sampleApp2/static/js/main.41917f8b.chunk.js"
+  },
+  {
+    "revision": "8a945104b8abb389069c",
+    "url": "/sampleApp2/static/js/runtime~main.c031a5ac.js"
+  },
+  {
+    "revision": "bf595043d295660f05c3",
+    "url": "/sampleApp2/static/js/2.614ca0f9.chunk.js"
+  },
+  {
+    "revision": "da5d1d8df530b83cf6c2711e3c53a9a8",
+    "url": "/sampleApp2/static/media/question.da5d1d8d.svg"
+  },
+  {
+    "revision": "855044fc23622aaf0e6b2960fabfc1d8",
+    "url": "/sampleApp2/static/media/circledeeppurple.855044fc.svg"
+  },
+  {
+    "revision": "708617a804923c668c6cc97544ab56dd",
+    "url": "/sampleApp2/static/media/circlesoftgreen.708617a8.svg"
+  },
+  {
+    "revision": "025e966dc0ed1005419d4d97678278b2",
+    "url": "/sampleApp2/static/media/circlegreen.025e966d.svg"
+  },
+  {
+    "revision": "5154e5e9f7fec05384c408457017c6de",
+    "url": "/sampleApp2/static/media/circlered.5154e5e9.svg"
+  },
+  {
+    "revision": "4477d6491cfd7523de3282c83adaefc2",
+    "url": "/sampleApp2/static/media/position.4477d649.svg"
+  },
+  {
+    "revision": "bef3aabba1d2bf71eded84320d9d7df5",
+    "url": "/sampleApp2/static/media/user.bef3aabb.svg"
+  },
+  {
+    "revision": "a3ba413fcd529462c15dba2b7c99eb3a",
+    "url": "/sampleApp2/static/media/location.a3ba413f.svg"
+  },
+  {
+    "revision": "4111022a25a5869bdc34ee340d106298",
+    "url": "/sampleApp2/static/media/home.4111022a.svg"
+  },
+  {
+    "revision": "e0d1c43d6075cff16ba74134dfa80354",
+    "url": "/sampleApp2/static/media/buildings.e0d1c43d.svg"
+  },
+  {
+    "revision": "b654708dcfc110098a14f4efa7161eb0",
+    "url": "/sampleApp2/static/media/road.b654708d.svg"
+  },
+  {
+    "revision": "b1fbf1c90f70ab56f3f3b359ba137d44",
+    "url": "/sampleApp2/static/media/rating.b1fbf1c9.svg"
+  },
+  {
+    "revision": "cedba72b0a60fec981f1b81b811b901a",
+    "url": "/sampleApp2/static/media/airport.cedba72b.svg"
+  },
+  {
+    "revision": "db5b99181257fefd4bc4f2c5934d7682",
+    "url": "/sampleApp2/static/media/timer.db5b9918.svg"
+  },
+  {
+    "revision": "197c485d19b2f4638f6a9243a861a208",
+    "url": "/sampleApp2/static/media/map.197c485d.svg"
+  },
+  {
+    "revision": "b80e98fc5a542c62902313258d9448c1",
+    "url": "/sampleApp2/index.html"
+  }
+];
